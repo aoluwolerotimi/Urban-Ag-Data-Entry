1844666903a3e5e72cdc4380738207c660d03cfc
aoluwolerotimi <a.oluwolerotimi@gmail.com>
2024-07-08 15:19:39 -0400

    As at july 8, 11 am

--- Code.js
+++ Code.js
@@ -2,9 +2,12 @@ function doGet() {
     return HtmlService.createHtmlOutputFromFile("main")
   }
   
-  function storeHarvest(harvest){
-  let ws=SpreadsheetApp.getActiveSpreadsheet();
-  let harvestSS =ws.getSheetByName("Harvest Tracking");
-  harvestSS.appendRow([harvest.harvestdate, harvest.source, harvest.crop,,,,,,])
+  function storeHarvest(harvest) {
+    let ws = SpreadsheetApp.getActiveSpreadsheet();
+    let harvestSS = ws.getSheetByName("Harvest Tracking");
   
+    // Iterate over each crop and create a new row for each one
+    harvest.crops.forEach(function(crop) {
+      harvestSS.appendRow([harvest.harvestdate, harvest.source, crop]);
+    });
   }
